18195acc5b3b2ef2d9df0cff2c0811c40d1c9e63
Marianne Jouffroy <marianne.jouffroy@hotmail.fr>
2025-09-07 22:47:40 +0200

    add config options and image assets

--- manifest.config.ts
+++ manifest.config.ts
@@ -3,14 +3,22 @@ import pkg from './package.json'
 
 export default defineManifest({
     manifest_version: 3,
-    name: pkg.name,
+    name: "__MSG_extensionName__",
+    short_name: "PinSight",
+    description: "__MSG_extensionDescription__",
     version: pkg.version,
     icons: {
-        48: "public/logo.png",
+        16: "public/icon_16.png",
+        32: "public/icon_32.png",
+        48: "public/icon_48.png",
+        128: "public/icon_128.png",
     },
     action: {
         default_icon: {
-            48: "public/logo.png",
+            16: "public/icon_16.png",
+            32: "public/icon_32.png",
+            48: "public/icon_48.png",
+            128: "public/icon_128.png",
         },
         default_popup: "src/popup/index.html",
     },
@@ -25,4 +33,5 @@ export default defineManifest({
         service_worker: "src/service-workers/background.ts",
     },
     permissions: ["storage", "tabs"],
+    default_locale: "en",
 });
